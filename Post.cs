4dce97efe9f2ad0fa3ab8ff652bc9154b0f96bfe
henry701 <henry_tuori@hotmail.com>
2018-09-22 14:40:29 -0300

    Remove redundant Date

--- Post.cs
+++ Post.cs
@@ -8,15 +8,15 @@ namespace Models
     public class Post : BaseEntity, ILikeAware
     {
         public User Poster { get; set; }
-        public System.DateTime date { get; set; }
+
         public GeoJsonPoint<GeoJson2DGeographicCoordinates> Position { get; set; }
         public IPAddress Ip { get; set; }
 
         public string Title { get; set; }
         public string Text { get; set; }
         public IList<FileReference> FileReferences { get; set; }
-        public IList<Comment> Comments { get; set; }
 
+        public IList<Comment> Comments { get; set; }
         public uint Likes { get; set; }
     }
 }
