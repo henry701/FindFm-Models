b4c15b5b97477a1e743aacd4b3fb3900920b0810
henry701 <henry_tuori@hotmail.com>
2018-10-06 10:10:35 -0300

    Remove CreationDate from comment

--- Comment.cs
+++ Comment.cs
@@ -9,6 +9,5 @@ namespace Models
         public User Commenter { get; set; }
         public string Text { get; set; }
         public ISet<ObjectId> Likes { get; set; }
-        public DateTime CreationDate { get; set; }
     }
 }
